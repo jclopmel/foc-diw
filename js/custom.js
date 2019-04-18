966cf0e4da9f825ec78276f129be02f82e7437db
jclopmel <jclm1988@gmail.com>
2019-04-18 19:31:28 +0200

    activacion jquery

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$("body").ready(function(){
+/*
+	$("body").on("mouseenter", "img", function(){
+		$(this).addClass("hoverImg");
+	})
+
+	$("body").on("mouseleave", "img", function(){
+		$(this).removeClass("hoverImg");
+	})
+*/
+})
